92cbd0e3d56f238319bb755115fc37050995dc23
Owner <luis333.123@gmail.com>
2023-08-12 11:08:19 -0500

    Creacion de main

---
+++ src/package1/Prueba.java
@@ -0,0 +1,8 @@
+package src.package1;
+
+public class Prueba {
+
+    public void test(){
+        System.out.printf("Cualquier string");
+    }
+}
---
+++ src/package2/Main.java
@@ -0,0 +1,12 @@
+package src.package2;
+
+import src.package1.Prueba;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        prueba.test();
+    }
+
+}
