c0fe8783388772f33bbce8b44ff1c762eae5bce1
Fercho <luis333.123@gmail.com>
2023-08-19 11:00:04 -0500

    Patrones de diseño

--- src/package2/Main.java
+++ src/package2/Main.java
@@ -1,12 +1,20 @@
 package src.package2;
 
 import src.package1.Prueba;
+import src.singleton.Singleton;
+import src.builder.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Prueba prueba = new Prueba();
-        prueba.test();
+
+        Singleton singleton = Singleton.getInstance();
+
+        Empleado empleado = Empleado.builder()
+                .id(123)
+                .nombre("Fernando")
+                .build();
+        System.out.println(empleado);
     }
 
 }
